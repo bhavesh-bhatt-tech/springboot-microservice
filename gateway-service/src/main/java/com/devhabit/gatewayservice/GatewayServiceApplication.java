df905a515d6c6977132f395f325b81c02b25314c
bhavesh-bhatt-tech <techcloudocean@gmail.com>
2025-09-02 18:30:09 +0530

    pom file changes
    pom file changes

--- gateway-service/src/main/java/com/devhabit/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/devhabit/gatewayservice/GatewayServiceApplication.java
@@ -1,15 +1,21 @@
 package com.devhabit.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
+		SpringApplication app = new SpringApplication(GatewayServiceApplication.class);
+		app.setWebApplicationType(WebApplicationType.REACTIVE); 
+		ConfigurableApplicationContext ctx = app.run(args);
+		//SpringApplication.run(GatewayServiceApplication.class, args);
+
 	}
 
 }
